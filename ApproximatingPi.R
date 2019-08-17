c73ec76479d163c2d1e818f9fb2a78cd63e8bb13
justinklumpkin <justinklumpkin@gmail.com>
2019-08-17 14:22:24 -0400

    Create ApproximatingPi.R
    initial commit
    returns pi, takes number of darts as parameter

---
+++ ApproximatingPi.R
@@ -0,0 +1,22 @@
+
+approximate_pi<-function(darts=500){
+dx <- runif(darts, -1,1)
+dy <- runif(darts,-1,1)
+cbind(x,y) # this is just so you can see it
+
+
+
+incircle<- 0
+for(i in 1:darts){
+  if(dx[i]**2 +dy[i]**2 <= 1){
+    incircle<- incircle+1
+  }
+}
+
+areaOfSquare<- 4
+pi1<- incircle/darts *areaOfSquare
+pi1
+incircle
+return(pi1)
+}
+
