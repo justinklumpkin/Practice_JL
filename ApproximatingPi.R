4ad79b4343ed7bc3ca4baba9c34f9fcf1db88d2c
Aishwarya B <aishbsk@gmail.com>
2019-08-17 14:34:53 -0400

    Update ApproximatingPi.R
    execute commit

--- ApproximatingPi.R
+++ ApproximatingPi.R
@@ -1,4 +1,4 @@
-
+#hello fellow person 
 approximate_pi<-function(darts=500){
 dx <- runif(darts, -1,1)
 dy <- runif(darts,-1,1)
@@ -9,14 +9,14 @@ cbind(x,y) # this is just so you can see it
 incircle<- 0
 for(i in 1:darts){
   if(dx[i]**2 +dy[i]**2 <= 1){
-    incircle<- incircle+1
+    Incircle<- incircle+1
   }
 }
 
 areaOfSquare<- 4
-pi1<- incircle/darts *areaOfSquare
+pi1<- incircle/darts *areaofSquare
 #pi1
 #incircle
-return(pi1)
+return(pi2)
 }
 
